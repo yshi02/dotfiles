f6bb251ffd988b6783d6d0ed9e499e28148148dd
Yufei Shi <86854844+yshi02@users.noreply.github.com>
2026-06-09 13:20:54 -0400

    shellrc: rename aliases ssj* to sshj*

--- dot_shellrc/aliases.sh
+++ dot_shellrc/aliases.sh
@@ -1,3 +1,10 @@
 # Common aliases
 alias ls="ls --color=auto"
 alias grep="grep --color=auto"
+
+# SSH but using proxy jump
+alias sshj="PROXY_JUMP=1 ssh"
+alias sshj-tunnel="PROXY_JUMP=1 ssh-tunnel"
+
+# Wrap tmux to avoid issues with direnv environment loading
+alias tmux='direnv exec / tmux'
